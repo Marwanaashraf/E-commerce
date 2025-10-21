6280f5009eb50c93fc935bf1118bac53db76ba4e
Marwanaashraf <marwanelmaghraby45@gmail.com>
2025-10-21 20:40:34 +0300

    cansel forgetPasword

--- src/Pages/auth/ForgetPassword/ForgetPassword.jsx
+++ src/Pages/auth/ForgetPassword/ForgetPassword.jsx
@@ -39,7 +39,7 @@ export default function ForgetPassword() {
     }
   }
   function canselProcess() {
-    navigate("/login");
+    navigate("/");
   }
   let formik = useFormik({
     initialValues: {
